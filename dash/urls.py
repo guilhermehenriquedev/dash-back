8225fbb8f0be1b8b0785098bb59f55de6edb186a
Guilherme Henrique <guilhermebass27@gmail.com>
2022-05-03 18:22:18 -0300

    Cria arquitetura e design para receber as api em csv

--- dash/urls.py
+++ dash/urls.py
@@ -1,27 +1,15 @@
-import debug_toolbar
-from django.contrib import admin
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from django.views.generic import TemplateView
 from django.urls import path, include
 from dash.core.views import (
     homeviews
 )
 
-#from rest_framework.authtoken.views import obtain_auth_token
-#from rest_framework_simplejwt.views import (
-#    TokenObtainPairView,
-#    TokenRefreshView,
-#)
-
 router = DefaultRouter()
 
-#router.register(r'exchanges', exchanges.ExchangeViewSet, basename='exchanges') #exchanges/criptos/
+#router.register(r'nome_rota', view.funcao, basename='name')
 
 urlpatterns = [
     path('', homeviews.index, name="index"),
     path('api/', include(router.urls)),
-    #path('api-token-auth/', obtain_auth_token, name='permanent_auth_token'),
-    #path('jwt-token/', TokenObtainPairView.as_view(), name='jwt_auth_token_obtain'),
-    #path('jwt-token/refresh/', TokenRefreshView.as_view(), name='jwt_auth_token_refresh')
 ]
